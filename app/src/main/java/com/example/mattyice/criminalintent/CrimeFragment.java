867f20d81598547566ec4d103c32bb687cee86d1
knerr <knerr@susqu.edu>
2015-02-26 10:40:55 -0500

    For challenge 6. Its the challenge at the end of chapter 8.

--- app/src/main/java/com/example/mattyice/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/example/mattyice/criminalintent/CrimeFragment.java
@@ -50,7 +50,7 @@ public class CrimeFragment extends Fragment {
         });
 
         mDateButton = (Button)v.findViewById(R.id.crime_date);
-        mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setText(android.text.format.DateFormat.format("EEEE, MMM dd, yyyy" ,mCrime.getDate()));
         mDateButton.setEnabled(false);
 
         mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
