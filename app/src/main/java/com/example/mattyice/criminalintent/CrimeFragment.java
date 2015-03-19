92569423ce9b37ccac11152fa05bf9639690f52e
knerr <knerr@susqu.edu>
2015-03-19 11:15:19 -0400

    Challenge 7, adding the timePicker dialog

--- app/src/main/java/com/example/mattyice/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/example/mattyice/criminalintent/CrimeFragment.java
@@ -1,5 +1,8 @@
 package com.example.mattyice.criminalintent;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
@@ -12,10 +15,17 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 
+import java.util.Date;
+import java.util.UUID;
+
 /**
  * Created by Matty Ice on 2/19/2015.
  */
 public class CrimeFragment extends Fragment {
+    public static final String EXTRA_CRIME_ID = "com.example.mattyice.criminalintent.crime_id";
+    private static final String DIALOG_DATE = "info";
+    public static final int REQUEST_INPUT = 0;
+
     private Crime mCrime;
     private EditText mTitleField;
     private Button mDateButton;
@@ -24,7 +34,8 @@ public class CrimeFragment extends Fragment {
     @Override
     public  void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        mCrime = new Crime();
+        UUID crimeId = (UUID)getArguments().getSerializable(EXTRA_CRIME_ID); //Way to call the contained value within the arguments
+        mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
     }
 
     @Override
@@ -32,6 +43,7 @@ public class CrimeFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_crime, parent, false);
 
         mTitleField = (EditText)v.findViewById(R.id.crime_title);
+        mTitleField.setText(mCrime.getTitle());
         mTitleField.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -50,10 +62,19 @@ public class CrimeFragment extends Fragment {
         });
 
         mDateButton = (Button)v.findViewById(R.id.crime_date);
-        mDateButton.setText(android.text.format.DateFormat.format("EEEE, MMM dd, yyyy" ,mCrime.getDate()));
-        mDateButton.setEnabled(false);
+        updateDate();
+        mDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getActivity().getSupportFragmentManager();
+                DateOrTimeFragment dialog = DateOrTimeFragment.newInstance(mCrime.getDate());
+                dialog.setTargetFragment(CrimeFragment.this, REQUEST_INPUT);
+                dialog.show(fm, DIALOG_DATE);
+            }
+        });
 
         mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setChecked(mCrime.isSolved());
         mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -66,6 +87,31 @@ public class CrimeFragment extends Fragment {
         return v;
 
 
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (resultCode != Activity.RESULT_OK) return;
+        if (requestCode == REQUEST_INPUT) {
+            Date date = (Date)data.getSerializableExtra(DateOrTimeFragment.EXTRA_DATE);
+            mCrime.setDate(date);
+            updateDate();
+        }
+    }
+
+    private void updateDate() {
+        mDateButton.setText(android.text.format.DateFormat.format("EEEE, MMM dd, yyyy - K m", mCrime.getDate()));
+    }
+
+    //Used to create a new instance of a fragment WITH included arguments
+    public static CrimeFragment newInstance(UUID crimeId) {
+        Bundle args = new Bundle();
+        args.putSerializable(EXTRA_CRIME_ID, crimeId);
+
+        CrimeFragment fragment = new CrimeFragment();
+        fragment.setArguments(args);
+
+        return fragment;
     }
 
 
